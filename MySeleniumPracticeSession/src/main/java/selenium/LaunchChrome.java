ecc6e010656e534915a5a5f334a42f81d033fd2b
Poorna <poorna@Poornas-MacBook-Pro.local>
2021-05-30 08:45:43 -0400

    adding practice session

---
+++ MySeleniumPracticeSession/src/main/java/selenium/LaunchChrome.java
@@ -0,0 +1,15 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	 public static void main(String[] args) {
+		 
+		 System.setProperty("webdriver.chrome.driver", "/Users/poorna/chromedriver/chromedriver");
+			WebDriver driver = new ChromeDriver();
+			driver.get("http://www.google.com");
+			System.out.println(driver.getTitle());
+	 }
+
+}
